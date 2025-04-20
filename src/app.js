28a68969b339cc03c865e2722abf99103997fc48
Francisco García <g.garciafran201@gmail.com>
2025-04-20 17:35:58 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
-import "bootstrap";
-import "./style.css";
-
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  function generarExcusa() {
+    const who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+    const action = ['ate', 'peed', 'crushed', 'broke'];
+    const what = ['my homework', 'my phone', 'the car'];
+    const when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+    const whoRandom = who[Math.floor(Math.random() * who.length)];
+    const actionRandom = action[Math.floor(Math.random() * action.length)];
+    const whatRandom = what[Math.floor(Math.random() * what.length)];
+    const whenRandom = when[Math.floor(Math.random() * when.length)];
+
+    return `${whoRandom} ${actionRandom} ${whatRandom} ${whenRandom}.`;
+  }
+
+  const excusa = generarExcusa();
+  document.getElementById("excuse").textContent = excusa;
 };
